189a68e111359f6c2c056a79856ef55848066256
mikepreston17 <michael.n.preston@gmail.com>
2023-01-23 22:38:50 -0600

    ++ zeroes page

---
+++ Pages/Zeroes/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace nugsnet6.Pages.Zeroes
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
