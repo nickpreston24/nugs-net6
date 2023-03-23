74c102ae4f072faf9dedc014eee8e7a493894d56
mikepreston17 <michael.n.preston@gmail.com>
2023-03-23 15:12:54 -0500

    ++ install script for local Ubuntu machines

---
+++ install-dotnet.sh
@@ -0,0 +1,19 @@
+
+# This contact's MS's version of dotnet for ubuntu
+# Explanation is here: https://learn.microsoft.com/en-us/dotnet/core/install/linux-ubuntu#2204-microsoft-package-feed
+
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk-7.0
+sudo apt-get install -y aspnetcore-runtime-7.0
+
+## these are to be removed after installation
+sudo apt autoremove aspnetcore-targeting-pack-6.0 dotnet-apphost-pack-6.0 dotnet-targeting-pack-6.0 gir1.2-snapd-1 libllvm13 libllvm13:i386
+https://github.com/harriris/external-ballistics-js.git
+echo "here'https://github.com/harriris/external-ballistics-js.gits the runtimes & sdks (if any):"
+
+dotnet --list-runtimes
+dotnet --list-sdks
