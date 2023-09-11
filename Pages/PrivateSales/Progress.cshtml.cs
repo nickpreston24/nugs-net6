0ae9fef03a2d9f9de329ccf52db9436443cde8d0
mikepreston17 <michael.n.preston@gmail.com>
2023-09-10 23:55:18 -0500

    ++ kanban

---
+++ Pages/PrivateSales/Progress.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace nugsnet6.Pages.PrivateSales;
+
+public class Progress : PageModel
+{
+    public object blah { get; set; }
+    public void OnGet()
+    {
+        
+    }
+}
