d5767d0ecb4b30368e0fed569e21004dd9633f8e
mikepreston17 <michael.n.preston@gmail.com>
2024-03-04 21:44:18 -0600

    added custom footer Hydro components

---
+++ Pages/Components/HydroFooter.cshtml.cs
@@ -0,0 +1,10 @@
+using Hydro;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace nugsnet6.Pages.Components;
+
+[HtmlTargetElement("hydro-footer")]
+public class HydroFooter : HydroView
+{
+}
---
+++ Pages/Shared/LockNLoadoutFooter.cshtml.cs
@@ -0,0 +1,9 @@
+using Hydro;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+// using CodeMechanic.UI.Hydro;
+namespace nugsnet6.Pages.Shared;
+
+[HtmlTargetElement("locknloadout-footer")]
+public class LockNLoadoutFooter : HydroView
+{
+}
